814051b895568be93555141bfcbf09b5e1e43f46
saunaik19 <saunaik19@gmail.com>
2021-06-08 02:37:41 +0530

    Internationalization

--- src/main/java/com/saurabh/example/restful/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/saurabh/example/restful/restfulwebservices/controller/HelloWorldController.java
@@ -1,13 +1,21 @@
 package com.saurabh.example.restful.restfulwebservices.controller;
 
 import com.saurabh.example.restful.restfulwebservices.dto.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 //@Controller
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/hello-world")
     public String hellOWorld() {
@@ -21,5 +29,11 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean/{name}")
     public HelloWorldBean hellOWorldBeanPathVariable(@PathVariable("name") String name) {
         return new HelloWorldBean("Hello World "+name);
+    }
+
+    @GetMapping("/hello-world-interN")
+    public String hellOWorldInternationalized(@RequestHeader(name = "Accept-Language",required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message",null,"Default message",locale);
+
     }
 }
